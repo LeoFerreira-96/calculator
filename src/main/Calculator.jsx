d91d192c031299616574f94cc9441e3508180ec3
Leonardo Ferreira <54919952+LeoFerreira-96@users.noreply.github.com>
2022-12-13 19:59:20 +0000

    modificando e atualizando de class para function

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,103 +1,71 @@
-/*eslint-disable no-eval */
-import React, { Component } from 'react'
+/* eslint-disable react/react-in-jsx-scope */
+import React, { useState } from 'react'
+import Button from '../components/Button'
+import Display from '../components/Display'
 import './Calculator.css'
 
-import Display from '../components/Display'
-import Button from '../components/Button'
+const Calculator = () => {
+    
+    const [initialState, setInitialState] = useState({
+        clearDisplay: false,
+        operation: null,
+        firstValue: null,
+        secondValue: null,
+        current: 0,
+    })
 
-
-const initialState = {
-    displayValue: '0',
-    clearDisplay: false,
-    operation: null,
-    values: [0, 0],
-    current: 0
-}
-export default class Calculator extends Component {
-
-    state = ({ ...initialState })
-
-    constructor(props) {
-        super(props)
-
-        this.clearMemory = this.clearMemory.bind(this)
-        this.setOperation = this.setOperation.bind(this)
-        this.addDigit = this.addDigit.bind(this)
+    const clearMemory = () => {
+        setInitialState({ ...initialState })
     }
 
-    clearMemory() {
-        this.setState({ ...initialState })
+    const Operation = (operation) => {
+        console.log(operation)
     }
-
-    setOperation(operation) {
-        if (this.state.current === 0) {
-            this.setState({ operation, current: 1, clearDisplay: true })
-        }
-        else {
-            const equals = operation === '='
-            const currentOperation = this.state.operation
-
-            const values = [...this.state.values]
             
-            try {
-                values[0] = eval(`${values[0]} ${currentOperation } ${values[1]}`)
+    const addDigit = (n) => {
+        console.log(n)
+        if(initialState.operation === null) {
+            if(initialState.firstValue === null) {
+                console.log(n)
+            }else{
+                console.log(n)
             }
-            catch(e) {    
-                values[1] = 0
+        }else {
+            if(initialState.secondValue === null) {
+                console.log(n)
+            }else{
+                console.log(n)
             }
-                
-
-            this.setState({
-                displayValue:values[0],
-                operation: equals ? null : operation,
-                current: equals ? 0:1,
-                clearDisplay:!equals,
-                values
-            })
-        }
-    }
-    addDigit(n) {
-        if (n === '.' && this.state.displayValue.includes('.')) {
-            return
-        }
-        console.log(this)
-        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
-        const currentValue = clearDisplay ? '' : this.state.displayValue
-        const displayValue = currentValue + n
-        this.setState({ displayValue, clearDisplay: false })
-
-        if (n !== '.') {
-            const i = this.state.current
-            const newValue = parseFloat(displayValue)
-            const values = [...this.state.values]
-            values[i] = newValue
-            this.setState({ values })
-            console.log(values)
         }
     }
 
-    render() {
-        return (
-            <div className="calculator">
-                <Display value={this.state.displayValue} />
-                <Button label="AC" click={this.clearMemory} triple />
-                <Button label="/" click={this.setOperation} operation />
-                <Button label="7" click={this.addDigit} />
-                <Button label="8" click={this.addDigit} />
-                <Button label="9" click={this.addDigit} />
-                <Button label="*" click={this.setOperation} operation />
-                <Button label="4" click={this.addDigit} />
-                <Button label="5" click={this.addDigit} />
-                <Button label="6" click={this.addDigit} />
-                <Button label="-" click={this.setOperation} operation />
-                <Button label="1" click={this.addDigit} />
-                <Button label="2" click={this.addDigit} />
-                <Button label="3" click={this.addDigit} />
-                <Button label="+" click={this.setOperation} operation />
-                <Button label="0" click={this.addDigit} double />
-                <Button label="." click={this.addDigit} />
-                <Button label="=" click={this.setOperation} operation />
-            </div>
-        )
-    }
+    return (
+    <div className="calculator">
+    <Display value={initialState.current} />
+    <Button label="AC" click={clearMemory} triple />
+    <Button label="/" click={Operation} operation />
+    <Button label="7" click={addDigit} />
+    <Button label="8" click={addDigit} />
+    <Button label="9" click={addDigit} />
+    <Button label="*" click={Operation} operation />
+    <Button label="4" click={addDigit} />
+    <Button label="5" click={addDigit} />
+    <Button label="6" click={addDigit} />
+    <Button label="-" click={Operation} operation />
+    <Button label="1" click={addDigit} />
+    <Button label="2" click={addDigit} />
+    <Button label="3" click={addDigit} />
+    <Button label="+" click={Operation} operation />
+    <Button label="0" click={addDigit} double />
+    <Button label="." click={addDigit} />
+    <Button label="=" click={Operation} operation />
+</div>)
 }
+    
+
+
+export default Calculator;
+
+
+
+
